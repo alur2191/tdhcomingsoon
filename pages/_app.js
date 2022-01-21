562145dd70e6552c3bf99145abc4e8aa05eed1b4
dandavisjs <dandavisjs@protonmail.com>
2022-01-21 07:22:15 -0800

    Avoid chaining critical requests

--- pages/_app.js
+++ pages/_app.js
@@ -19,7 +19,7 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <>
-      <Script
+      <Script 
         strategy="afterInteractive"
         src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
       />

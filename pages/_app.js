dda91d4bec7401ae6e9882a65d31d551fb9b6282
dandavisjs <dandavisjs@protonmail.com>
2022-01-20 16:54:40 -0800

    Google analytics added

--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,30 @@
-import { useEffect } from 'react'
-import { useRouter } from 'next/router'
-
 import '../styles/globals.css'
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
-  const router = useRouter()
-
-  useEffect(() => {
-    const handleRouteChange = url => {
-      window.gtag('config', process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS, {
-        page_path: url,
-      })
-    }
-    router.events.on('routeChangeComplete', handleRouteChange)
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange)
-    }
-  }, [router.events])
-
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=XXXXXXXXX`}
+      />
+      <Script
+        id="g-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-XXXXXXX', {
+            page_path: window.location.pathname,
+          });
+        `,
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
--- pages/_document.js
+++ pages/_document.js
@@ -10,22 +10,7 @@ class MyDocument extends Document {
                     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;600&display=swap" rel="stylesheet"
                     />
                     <meta name="description" content="TruckDriver.help предоставляем эффективный сервис по поиску работы, а также размещению вакансий на работу. "></meta>
-                    <script
-                        async
-                        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-                    />
-                    <script
-                        dangerouslySetInnerHTML={{
-                            __html: `
-                                window.dataLayer = window.dataLayer || [];
-                                function gtag(){dataLayer.push(arguments);}
-                                gtag('js', new Date());
-                                gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-                                page_path: window.location.pathname,
-                                });
-                            `,
-                        }}
-                    />
+                    
                 </Head>
                 <body>
                     <Main />

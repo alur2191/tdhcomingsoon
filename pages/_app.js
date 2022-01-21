d705ebb96e538170bb2d17c53a138a5b3fa89b1f
dandavisjs <dandavisjs@protonmail.com>
2022-01-20 16:59:49 -0800

    Google analytics refactoring

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
 import '../styles/globals.css'
+import "../styles/globals.css";
+import { useEffect } from "react";
 import Script from "next/script";
+import { useRouter } from "next/router";
+import * as gtag from "../lib/gtag";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <>
       <Script
